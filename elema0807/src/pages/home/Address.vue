1c98e27a5d1e65a10a7b8963cf56e6b1c287fe7a
chao <1547631973@qq.com>
2018-08-09 10:28:19 +0800

    zuixin

--- elema0807/src/pages/home/Address.vue
+++ elema0807/src/pages/home/Address.vue
@@ -22,55 +22,55 @@
         </div>
         <ul>
             <li class="addressList" v-for="(address,index) in addressList" :key="index" @click="selectAction(address)">
-                <p>{{address.name}}</p>
-				<p>{{address.address}}</p>
-                <span>{{address.distance}}</span>
+                <div class="detailed-address">
+                    <p>{{address.name}}</p>
+				    <p>{{address.address}}</p>
+                </div>
+                <div>{{address.distance}}</div>
             </li>
         </ul>
     </div>
 </template>
 
 <script>
-import { getHomeAddressData } from '../../services/homedata.js'
-import Vuex from 'vuex'
+import { getHomeAddressData } from "../../services/homedata.js";
+import Vuex from "vuex";
 export default {
-    data(){
-        return{
-            value: '',
-            addressList: []
-        }
+  data() {
+    return {
+      value: "",
+      addressList: []
+    };
+  },
+  computed: {
+    ...Vuex.mapState({
+      location: state => state.location.locValue
+    })
+  },
+  methods: {
+    ...Vuex.mapActions({
+      userSelectLocation: "location/userSelectLocation"
+    }),
+    searchAction() {
+      let { longitude, latitude } = this.location;
+      getHomeAddressData(this.value, latitude, longitude, 20).then(result => {
+        console.log(result);
+        this.addressList = result;
+      });
     },
-    computed: {
-		...Vuex.mapState({
-			location: state=>state.location.locValue
-		})
-	},
-    methods:{
-        ...Vuex.mapActions({
-			userSelectLocation: 'location/userSelectLocation'
-        }),
-        searchAction(){
-            let {longitude, latitude} = this.location;
-			getHomeAddressData(this.value, latitude, longitude, 20)
-			.then(result=>{
-				console.log(result);
-				this.addressList = result;
-			})
-        },
-        selectAction(address){
-			console.log(address);	
-			this.userSelectLocation({
-				longitude: address.longitude,
-				latitude: address.latitude
-			});
-			this.$router.back();
-		},
-        backAction(){
-            this.$router.back();
-        }
+    selectAction(address) {
+      console.log(address);
+      this.userSelectLocation({
+        longitude: address.longitude,
+        latitude: address.latitude
+      });
+      this.$router.back();
     },
-    
-}
+    backAction() {
+      this.$router.back();
+    }
+  }
+};
 </script>
 
 <style>
@@ -78,6 +78,9 @@ export default {
   width: 100%;
   height: 44px;
   background-image: linear-gradient(90deg, #0af, #0085ff);
+  position: sticky;
+  top: 0px;
+  z-index: 10;
 }
 .my {
   font-size: 18px;
@@ -100,41 +103,56 @@ export default {
 .arrow-left img {
   width: 12px;
 }
-.addressBox{
-    display: flex;
-    background-color: #fff;
+.addressBox {
+  display: flex;
+  background-color: #fff;
 }
-.city{
-    align-items: center;
-    padding-left: 15px;
-    display: flex;
+.city {
+  align-items: center;
+  padding-left: 15px;
+  display: flex;
 }
-.city span{
-    width: 40px;
-    color: #333;
-    margin-right: 4px;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    text-align: center;
+.city span {
+  width: 40px;
+  color: #333;
+  margin-right: 4px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  text-align: center;
 }
-.addressInp{
-    position: relative;
-    padding: 10px 15px;
-    flex: 1;
+.addressInp {
+  position: relative;
+  padding: 10px 15px;
+  flex: 1;
 }
-.addressInp svg{
-    position: absolute;
-    width: 14px;
-    height: 14px;
-    left: 21px;
-    top: 22px;
+.addressInp svg {
+  position: absolute;
+  width: 14px;
+  height: 14px;
+  left: 21px;
+  top: 22px;
 }
 .index-2V7cn_0 {
-    width: 100%;
-    line-height: 9px;
-    color: #999;
-    padding: 9px 18px 9px 25px;
-    background: #f2f2f2;
+  width: 100%;
+  line-height: 9px;
+  color: #999;
+  padding: 9px 18px 9px 25px;
+  background: #f2f2f2;
+}
+.addressList {
+  font-size: 0.32rem;
+  background-color: #fff;
+  padding: 11px 15px;
+  color: #2a2a2a;
+  display: flex;
+}
+.detailed-address {
+  flex: 1;
+}
+.detailed-address p:nth-child(1) {
+  font-weight: 700;
+  font-size: 15px;
+  color: #333;
 }
 </style>
